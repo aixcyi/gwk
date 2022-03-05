d202737e05818905c0baff32ada71c1b5be57ff2
aixcyi <75880483+aixcyi@users.noreply.github.com>
2022-03-05 17:38:21 +0800

    chore: 展开__init__.py的导入

--- gwk/records/__init__.py
+++ gwk/records/__init__.py
@@ -1,2 +1,11 @@
-from gwk.records.models import *
-from gwk.records.wrappers import *
+from gwk.records.models import (
+    Wish,
+    PlayerPool,
+    PlayerShelf,
+    map_raw_to_uigf_j2,
+    map_raw_to_basic,
+)
+from gwk.records.wrappers import (
+    GenshinResponse,
+    RawCollector,
+)
