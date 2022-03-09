589cecc3097a62a20f9fa38dcecb14db16182ea5
aixcyi <75880483+aixcyi@users.noreply.github.com>
2022-03-09 23:34:05 +0800

    fix: 去除不存在的导入

--- gwk/records/__init__.py
+++ gwk/records/__init__.py
@@ -2,8 +2,6 @@ from gwk.records.models import (
     Wish,
     PlayerPool,
     PlayerShelf,
-    map_raw_to_uigf_j2,
-    map_raw_to_basic,
 )
 from gwk.records.wrappers import (
     GenshinResponse,
