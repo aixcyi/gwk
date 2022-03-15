bdf26c467870e41dcb80ad861c08076ce257bc02
aixcyi <75880483+aixcyi@users.noreply.github.com>
2022-03-16 01:22:50 +0800

    refactor: 将demo中的迁移加合并到汇总记录，改为直接迁移为汇总记录

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -1,24 +1,14 @@
 from gwk.records.excel import save_as_uigf
-from gwk.records.models import Wish, migrate
+from gwk.records.models import migrate
 
 
 def main(uid):
     with open(f'./ggr_{uid}.json', 'r', encoding='UTF-8') as f:
         old = migrate(f)
 
-    new = Wish()
-    with open(f'./records_{uid}.json', 'r', encoding='UTF-8') as f:
-        new.load(f)
-
-    new.merging_check = False
-    new += old
-    new.merging_check = True
-    new.sort(key=lambda r: (r['uigf_gacha_type'], r['time'], r['id']))
-    new.deduplicate()
-
-    with open(f'./migration_{uid}.json', 'w', encoding='UTF-8') as f:
-        new.dump(f)
-    save_as_uigf(new, f'./migration_{uid}.xlsx')
+    with open(f'./records_{uid}.json', 'w', encoding='UTF-8') as f:
+        old.dump(f)
+    save_as_uigf(old, f'./records_{uid}.xlsx')
 
 
 if __name__ == '__main__':
